2ba415406aa008cb682de5d145ab08372b7ccb8d
Dev@CryptoSDK <dev@cryptosdk.de>
2024-06-24 23:46:06 +0200

    sign

--- src/main/java/com/example/updater/MyApp.java
+++ src/main/java/com/example/updater/MyApp.java
@@ -6,11 +6,13 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 import java.io.FileOutputStream;
+import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.KeyFactory;
-import java.security.PublicKey;
-import java.security.Signature;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
@@ -20,7 +22,7 @@ public class MyApp {
     //public static final int version = 20240623;
     public static int newVersion = -1;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SignatureException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException {
 
         OkHttpClient client = new OkHttpClient.Builder().build();
 
@@ -31,7 +33,7 @@ public class MyApp {
         System.out.println("Runs " + version);
     }
 
-    private static void runUpdate(OkHttpClient client) throws IOException {
+    private static void runUpdate(OkHttpClient client) throws IOException, SignatureException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException {
         Request request = new Request.Builder()
                 .url("https://raw.githubusercontent.com/user9209/java-autoupdater/master/_releases_/app_" + newVersion + ".jar")
                 .build();
@@ -45,14 +47,47 @@ public class MyApp {
         }
 
         // todo: verify signature
+        byte[] signature = downloadSignature(client);
+        Signature signEng = Signature.getInstance("SHA256withRSA");
+        signEng.initVerify(getPublicKey());
 
         FileOutputStream fos = new FileOutputStream("app_" + newVersion + ".jar");
         InputStream is = response.body().byteStream();
-        is.transferTo(fos);
+        MessageDigest sha3_512 = MessageDigest.getInstance("SHA3-512");
+        DigestInputStream dis = new DigestInputStream(is,sha3_512);
+        dis.transferTo(fos);
         fos.flush();
         fos.close();
         is.close();
+        dis.close();
 
+        signEng.update(sha3_512.digest());
+        if(!signEng.verify(signature)) {
+            Files.delete(Path.of("app_" + newVersion + ".jar"));
+        }
+    }
+
+    private static PublicKey getPublicKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String publicKey = "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAjNDHQtrtIOKGycIcPCP9gZEVwYHoziI5UdpHeELjOVk2vchUQtCzv/EgPKVY1lSMC864Y81KV0yK5zW2wHCsAb2E3CWa6EBx37bj/6jS5dQNZlJdVTEvWwPAaE9yhVvyilRjQ8TubzxOOsLTE0jHIPlB7sqlh2ZTQho9mjsiBD4E+EJwrMyc1+mjaemRYl4a7kxD4BRTOOSju3FWpKKHtSlrj0xADP8JWjthJkLZNTa+V3DfssJml/gtPTmUfF6l2tG6BspBb8xya4UlOEa0tfmgxJGVxY1+i7Cd1JbZ48j0HauSzWDdvha6uwHA/ApZNGXQR2TRNl4C/cUPYeh49h+KxUZL6xTlo5yIRF8kpV1OMeV7B3CV5p3C5C5pJnV5SY2sSrilNb7BcFa+GaySUIeY4MKMJcRbRhYs2CV6ChEffQZUbhdn+ap7iOdKNqVQ61ns2WL9uiFNk/6tBInSc9YmkAoWGGXZms0PLgucDbmhV73OsOPRrhvEgqFgnuGiaWc9B/wLVWgLjkZq2ZvOJwZoXsJxDa155NCXBBYUMW416lOsNCA+E3IMCswHpcv1VbzDKuXLY6BMVAUIlV/BmqrEQMC7basDa3NdBi3HNFekNVyLx7updr96Ig5CMr5SiLAwv+Dr90cStTIkdcUOvi18SkjzhDCXdCSiCz9tbPsCAwEAAQ";
+
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(keySpec);
+    }
+
+    private static byte[] downloadSignature(OkHttpClient client) throws IOException {
+        Request request = new Request.Builder()
+                .url("https://raw.githubusercontent.com/user9209/java-autoupdater/master/_releases_/app_" + newVersion + ".jar.sig")
+                .build();
+
+        Call call = client.newCall(request);
+        Response response = call.execute();
+
+        if(response.code() != 200) {
+            System.err.println("Download failed! " + response.code());
+            return new byte[0];
+        }
+        return response.body().bytes();
     }
 
     private static boolean checkUpdate(OkHttpClient client) {
@@ -79,7 +114,6 @@ public class MyApp {
             return false;
         }
     }
-
 
     public static PublicKey loadPublicKey(String base64PublicKey) throws Exception {
         byte[] keyBytes = Base64.getDecoder().decode(base64PublicKey);
--- src/main/java/com/example/updater/Sign.java
+++ src/main/java/com/example/updater/Sign.java
@@ -1,8 +1,14 @@
 package com.example.updater;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.security.PrivateKey;
-import java.security.Signature;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class Sign {
     public static byte[] sign(PrivateKey privateKey, byte[] data) throws Exception {
@@ -16,11 +22,64 @@ public class Sign {
         Signature signature = Signature.getInstance("SHA256withRSA");
         signature.initSign(privateKey);
 
+        MessageDigest sha3_512 = MessageDigest.getInstance("SHA3-512");
+        DigestInputStream dis = new DigestInputStream(is,sha3_512);
+
         byte[] buf = new byte[4096];
         int l;
-        while ((l = is.read(buf)) > 0) {
-            signature.update(buf);
+        while ((l = dis.read(buf)) > 0) {
+           // signature.update(buf,0,l);
+            // ignore data
         }
+        dis.close();
+
+        signature.update(sha3_512.digest());
+
         return signature.sign();
+    }
+
+    public static void main(String[] args) throws Exception {
+        PrivateKey privateKey;
+
+        if(!Files.exists(Path.of("prvkey.b64"))) {
+
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+
+            // Initialize the KeyPairGenerator with the key size (e.g., 2048 bits)
+            keyGen.initialize(4096);
+
+            // Generate the KeyPair (public and private keys)
+            KeyPair keyPair = keyGen.generateKeyPair();
+
+            // Extract the Public and Private Keys from the KeyPair
+            PublicKey publicKey = keyPair.getPublic();
+            privateKey = keyPair.getPrivate();
+
+            String publicKeyB64 = Base64.getEncoder().withoutPadding().encodeToString(publicKey.getEncoded());
+            String privateKeyB64 = Base64.getEncoder().withoutPadding().encodeToString(privateKey.getEncoded());
+
+            // Print the keys in Base64 encoding
+            System.out.println("Public Key: " + publicKeyB64);
+            System.out.println("Private Key: " + privateKeyB64);
+
+            Files.writeString(Path.of("pubkey.b64"), publicKeyB64);
+            Files.writeString(Path.of("prvkey.b64"), privateKeyB64);
+        }
+        else {
+            privateKey = loadPrivateKey(Files.readString(Path.of("prvkey.b64")));
+        }
+
+        FileInputStream fis = new FileInputStream("_releases_/app_20240626.jar");
+        byte[] sign = sign(privateKey,fis);
+        fis.close();
+
+        Files.write(Path.of("_releases_/app_20240626.jar.sig"), sign);
+    }
+
+    public static PrivateKey loadPrivateKey(String base64PublicKey) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(base64PublicKey);
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(spec);
     }
 }
